c3e6f0ce908d2b568418a97414480ee77f8ce009
MuhdSufi1409 <muhdsufi1409@gmail.com>
2015-03-24 09:26:20 +0800

    search

---
+++ CI/application/views/searchview.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Welcome to Eklinik</title>
+</head>
+<body>
+	<h1>Search Patient IC</h1>
+	<form action="#" method="post">
+			<b>Patient IC</b> <input name="#" type="text" size="20" maxlength="200" />
+			<input type="submit" value="Search"/>
+<br />
+<br />
+<table width="850" border="3" rules="all" cellpadding="3">
+
+	<tr>
+    <th>Patient Name</th>
+    <th>Patient IC</th>
+	<th>Patient Address</th>
+	<th>Patient Gender</th>
+    </tr>
+</form>
+</body>
+</html>
